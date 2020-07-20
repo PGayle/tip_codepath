dcbce4bae783bdaf1dac93a836f14a179d18edef
Paden  Gayle <padengayle@gmail.com>
2020-07-20 15:40:48 -0700

    Initial Commit

---
+++ tip_codepath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_codepath
+//
+//  Created by Paden Gayle on 7/20/20.
+//  Copyright © 2020 Paden Gayle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
